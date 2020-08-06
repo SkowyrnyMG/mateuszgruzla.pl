7ba7d0f525ecf549c79a5cee9941490973fbc7a9
Mateusz <m.gruzla@hotmail.com>
2020-08-07 00:30:50 +0200

    maintenance v4

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ import SEO from '../components/seo';
 const NotFoundPage = ({ location }) => {
   return (
     <Layout location={location} title='404'>
-      <SEO title='404: Not Found' description='not found' />
+      {/* <SEO title='404: Not Found' description='not found' /> */}
       <h1>Not Found</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </Layout>
