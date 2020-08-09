418fa268ba34c5886ee35673239e4470123b519f
Mateusz <m.gruzla@hotmail.com>
2020-08-09 16:52:10 +0200

    Final bugfixes with 404 page. For now I've deleted completely SEO component from use

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,6 @@ import Layout from '../components/layout';
 const NotFoundPage = () => {
   return (
     <Layout title='404'>
-      <SEO title='404: Not Found' />
       <h1>Not Found</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </Layout>
