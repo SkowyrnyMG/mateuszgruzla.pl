b8d1a6355f6b1240376063a7b1048a18da8e5aeb
Mateusz <m.gruzla@hotmail.com>
2020-08-09 12:42:58 +0200

    Resolved bug with 404 file

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { graphql } from 'gatsby';
 
 import Layout from '../components/layout';
-import SEO from '../components/seo';
 
-const NotFoundPage = ({ location }) => {
+const NotFoundPage = () => {
   return (
-    <Layout location={location} title='404'>
+    <Layout title='404'>
       <SEO title='404: Not Found' />
       <h1>Not Found</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
