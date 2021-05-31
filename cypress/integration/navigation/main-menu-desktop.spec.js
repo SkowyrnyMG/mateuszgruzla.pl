f032a9c29b17c00c5f0ea3d387114170a4d5afd9
Mateusz <m.gruzla@hotmail.com>
2021-05-31 23:21:25 +0200

    Main menu e2e main buttons tested

--- cypress/integration/navigation/main-menu-desktop.spec.js
+++ cypress/integration/navigation/main-menu-desktop.spec.js
@@ -7,6 +7,33 @@ describe('Main menu', () => {
   })
 
   it('should contain clickable main buttons', () => {
-    cy.log(':)')
+    cy.get('[data-testid="main-menu"]').within(() => {
+      cy.get('button').last().click();
+    });
+    cy.get('nav').should('exist').and('be.visible');
+
+    cy.get('[data-testid="main-menu"]').within(() => {
+      cy.get('button').eq(1).click();
+    });
+    cy.get('input').should('have.attr', 'placeholder').and('eq', 'Search').and('exist');
+    cy.get('button').contains(/close/i).click();
+
+    cy.get('[data-testid="main-menu"]').within(() => {
+      cy.get('button').eq(0).click();
+    });
+    cy.getCookie('theme')
+    .should('have.property', 'value', 'light');
+
+    cy.get('[data-testid="main-menu"]').within(() => {
+      cy.get('button').eq(0).click();
+    });
+    cy.getCookie('theme')
+    .should('have.property', 'value', 'dark');
+
+    cy.visit('/about');
+    cy.get('[data-testid="main-menu"]').within(() => {
+      cy.get('a').first().click();
+    });
+    cy.url().should('eq', 'http://localhost:8000/');
   });
 })
